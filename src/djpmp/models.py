8711dfc9ac0f209c5310f5f3f678fe35b68137f7
daijian <daijian1@qq.com>
2020-04-07 13:05:12 +0800

    update

--- src/djpmp/models.py
+++ src/djpmp/models.py
@@ -106,6 +106,7 @@ class HRCalendar(TimeStampedModel):
     ev = models.FloatField(default=0, choices=(
         (0, 0),
         (0.5, 0.5),
+        (1, 1),
         (1.5, 1.5),
         (2, 2)
     ))
