15e9d8ed931a6ad9fba1eade4264f0d8f7cc4566
daijian <daijian1@qq.com>
2020-04-14 22:17:35 +0800

    update

--- src/djpmp/forms.py
+++ src/djpmp/forms.py
@@ -8,6 +8,7 @@ from . import models as m
 
 
 class DateSpanForm(forms.Form):
+    company = forms.ModelChoiceField(None, required=True, label='公司')
     project = forms.ModelChoiceField(None, required=True, label='项目')
     start_date = forms.DateField(required=True, label='开始日期', widget=forms.DateInput(attrs={"type": "date"}))
     # end_date = forms.DateField(required=True, label='结束日期', widget=forms.SelectDateWidget)
@@ -22,9 +23,11 @@ class DateSpanForm(forms.Form):
         super().__init__(*args, **kwargs)
         user = get_request().user
         companie_ids = [x.company_id for x in user.companies.all()]
+        self.fields['company'].queryset = m.Company.objects.filter(pk__in=companie_ids).order_by('name')
         self.fields['project'].queryset = m.Project.objects.filter(company_id__in=companie_ids).order_by('name')
         self.fields['staffs'].queryset = m.Staff.objects.filter(company_id__in=companie_ids).order_by('name')
         self.initial = {
+            'company': self.fields['company'].queryset.last(),
             'project': self.fields['project'].queryset.last(),
             'staffs': self.fields['staffs'].queryset.all(),
             'start_date': datetime.datetime.now().strftime('%Y-%m-%d')
